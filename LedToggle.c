434cdb87253ea992617014e7196a69862c970c41
venkata-p2498 <81845583+venkata-p2498@users.noreply.github.com>
2021-04-03 11:08:15 +0530

    Update LedToggle.c

--- LedToggle.c
+++ LedToggle.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
-#define	led((volatile unsigned*)(0x80840020))
-#define	leddir((volatile unsigned*)(0x80840024))
-void main(void)
-{  
-  unsigned int	i,j;
-	*led=0x00; 
-   *leddir=0x03;
+#define	led ((volatile unsigned*)(0x80840020))
+#define	leddir ((volatile unsigned*)(0x80840024))
+void delay();
+int main()
+{
+  *leddir=0x03;
   while(1)
-   { 
-   // *led=0x00;
-   // for(i=0;i<600;i++)
-   // for(i=0;i<60000;i++);
-      *led=0x02;
-    for(j=0;j<600;j++)
-    for(i=0;i<60000;i++);
+  { //delay();
     *led=0x01;
-    for(j=0;j<600;j++)
-   for(i=0;i<60000;i++);
-   }
+    delay();
+    *led=0x02;
+    delay();
+    //*led=0x00;
   }
+}
+void delay()
+{
+  int i;
+  for(i=0;i<1000000;i++); 
+}
